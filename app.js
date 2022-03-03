934ced678d1a2f8975168ff1e25153c951c91a52
jciterceros <jciterceros@yahoo.com>
2022-03-03 09:34:47 -0300

    Update token

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
 // I need this id's and secrets should come from .env file.
-const CLIENT_ID = '1051303266738-2uru9v19cp89ga1f6a7r08qcoe70hj8f.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04H1-LL9228xgCgYIARAAGAQSNwF-L9IrqbAQuG254uBlQrP2gjSd69CcNitNaKt4TJFha0F9I-5-A4ok53l62g7mgtAtkFpebKw';
+const REFRESH_TOKEN = '';
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -22,7 +22,7 @@ async function sendMail() {
       service: 'gmail',
       auth: {
         type: 'OAuth2',
-        user: 'fernando@mxdrive.com.br',
+        user: 'nome@organizacao.com.br',
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
@@ -31,13 +31,13 @@ async function sendMail() {
     });
 
     const mailOptions = {
-      from: 'Fernando Terceros <fernando@mxdrive.com.br>',
-      to: 'cassio.omura@mxdrive.com.br',
-      cc: 'rodrigo@mxdrive.com.br',
-      co: 'jciterceros@gmail.com',
+      from: 'nome sobrenome <nome@organizacao.com.br>',
+      to: 'nome_destino@organizacao.com.br',
+      cc: 'nome_destino_em_copia@organizacao.com.br',
+      co: 'nome_destino_copia_oculta@organizacao.com.br',
       subject: 'Enviado pelo gmail Usando GoogleAPI',
-      text: 'Teste de envio de email com autenticacao de seguranca, quem diz que ao pode ser feito...',
-      html: '<h1>Teste de envio de email com autenticacao de seguranca, quem diz que ao pode ser feito...</h1>',
+      text: 'Teste de envio de email com autenticacao de seguranca',
+      html: '<h1>Teste de envio de email com autenticacao de seguranca</h1>',
       attachments:[
           {filename:'Liguagem.jpg',path:'./Liguagem.jpg'}
       ]
